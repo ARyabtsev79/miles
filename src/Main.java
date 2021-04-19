9f32fcb03e185c1225ea4c12fb32a048f800f987
ARyabtsev79 <ryabtsev79@gmail.com>
2021-04-19 12:51:22 +0300

    add miles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // стоимость билета в копейках
+        long ticketСost = 21_700_000;
+        long bonus = ticketСost / 20 / 100;
+        System.out.println(bonus);
+    }
+}
